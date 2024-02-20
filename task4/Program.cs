714fd9e6788dd7b590f3b4429e02d904300e12ac
Anna_skazka <smartupfit@yandex.ru>
2024-02-20 13:12:22 +1000

    Решили четвертую задачу

---
+++ task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Цифры числа " + N + " через запятую: ");
+int divider = 1;
+while (N / divider >= 10)
+{
+    divider *= 10;
+}
+while (divider > 0)
+{
+    int digit = N / divider;
+    Console.Write(digit);
+    N %= divider;
+    divider /= 10;
+    if (divider > 0)
+    {
+         Console.Write(", ");
+    }
+}
+
+
+
