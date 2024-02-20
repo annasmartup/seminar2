646289d772d7deef39a45f6743d0644e05f2254e
Anna_skazka <smartupfit@yandex.ru>
2024-02-20 12:42:06 +1000

    Сделали третье задание

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number / 10;
+int b = number % 10;
+if (a > b){
+    Console.Write(a);
+} else{
+    Console.Write(b);
+}
