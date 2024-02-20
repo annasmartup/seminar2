ee5449b605ee7f77300e3dfaa5b683beaa4f3d19
Anna_skazka <smartupfit@yandex.ru>
2024-02-20 12:34:57 +1000

    Сделали второе задание

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int x = -3;
+int y = 2;
+if (x > 0 && y > 0){
+    Console.Write("1");
+}else if (x < 0 && y > 0){
+    Console.Write("2");
+}else if (x < 0 && y < 0){
+    Console.Write("3");
+}else if (x > 0 && y < 0){
+    Console.Write("4");
+}else{
+    Console.Write("х и у не могут быть равны 0, введите другие координаты");
+}
